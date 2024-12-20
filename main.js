dde8462acde9731cd80f5cc107200adc7911b00f
elbaz-youssef <youssef.devworks@gmail.com>
2024-12-20 15:25:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const qrCodeRegionId = "reader";
+    
+const handleScanSuccess = (decodedText) => {
+    console.log("Scanned result: " + decodedText);
+    alert("Scanned result: " + decodedText);
+};
+
+const handleScanError = (error) => {
+    console.error("Scan error:", error);
+};
+
+const scanner = new Html5QrcodeScanner(qrCodeRegionId, {
+    fps: 10,
+    qrbox: 250,
+}, false);
+
+scanner.render(handleScanSuccess, handleScanError);
